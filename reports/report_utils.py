edbbb25d3e256dd56715938779a410e6a3c722fc
SaiRamC123-com <chutkeychethan@gmail.com>
2026-06-04 13:09:12 +0530

    Add get_summary function for participant data
    Added a function to summarize participant data including total count and averages for usage hours, anxiety, depression, and sleep.

--- reports/report_utils.py
+++ reports/report_utils.py
@@ -1,1 +1,23 @@
+import pandas as pd
 
+def get_summary(df):
+
+    summary = {
+
+        "Total Participants": len(df),
+
+        "Average Usage Hours":
+        round(df["Daily Usage Hours"].mean(),2),
+
+        "Average Anxiety":
+        round(df["Anxiety Score"].mean(),2),
+
+        "Average Depression":
+        round(df["Depression Score"].mean(),2),
+
+        "Average Sleep":
+        round(df["Sleep Hours"].mean(),2)
+
+    }
+
+    return summary
