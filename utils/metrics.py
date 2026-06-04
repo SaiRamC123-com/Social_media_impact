ec2fd7677d738cae3fe478538d94847fdb1ceadc
SaiRamC123-com <chutkeychethan@gmail.com>
2026-06-04 13:06:55 +0530

    Update metrics.py

--- utils/metrics.py
+++ utils/metrics.py
@@ -1,1 +1,24 @@
+def total_participants(df):
 
+    return len(df)
+
+def avg_usage(df):
+
+    return round(
+        df["Daily Usage Hours"].mean(),
+        2
+    )
+
+def avg_anxiety(df):
+
+    return round(
+        df["Anxiety Score"].mean(),
+        2
+    )
+
+def avg_depression(df):
+
+    return round(
+        df["Depression Score"].mean(),
+        2
+    )
