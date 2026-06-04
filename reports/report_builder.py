c4c0bb82a0b027259ea0bdf8ea2907763043f60f
SaiRamC123-com <chutkeychethan@gmail.com>
2026-06-04 13:10:20 +0530

    Update report_builder.py

--- reports/report_builder.py
+++ reports/report_builder.py
@@ -1,1 +1,62 @@
+from reports.report_utils import (
+    get_summary
+)
 
+from reports.report_insights import (
+    generate_ai_insights
+)
+
+from reports.pdf_generator import (
+    generate_pdf
+)
+
+from reports.excel_generator import (
+    generate_excel
+)
+
+def build_reports(df):
+
+    summary = get_summary(df)
+
+    insights = generate_ai_insights(df)
+
+    pdf_file = generate_pdf(
+        summary,
+        insights
+    )
+
+    excel_file = generate_excel(df)
+
+    csv_file = (
+        "reports/generated_reports/"
+        "mental_health_report.csv"
+    )
+
+    df.to_csv(
+        csv_file,
+        index=False
+    )
+
+    text_file = (
+        "reports/generated_reports/"
+        "ai_summary.txt"
+    )
+
+    with open(
+        text_file,
+        "w"
+    ) as file:
+
+        file.write(insights)
+
+    return {
+
+        "pdf": pdf_file,
+
+        "excel": excel_file,
+
+        "csv": csv_file,
+
+        "summary": text_file
+
+    }
