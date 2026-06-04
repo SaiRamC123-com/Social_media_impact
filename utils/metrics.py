318b2088c68a4fb6fb6fe79744bf5cb0af97855f
SaiRamC123-com <chutkeychethan@gmail.com>
2026-06-04 13:25:02 +0530

    Update metrics.py

--- utils/metrics.py
+++ utils/metrics.py
@@ -1,24 +1,59 @@
-def total_participants(df):
+def total_students(df):
 
     return len(df)
 
-def avg_usage(df):
+
+def avg_age(df):
 
     return round(
-        df["Daily Usage Hours"].mean(),
+        df["age"].mean(),
         2
     )
+
+
+def avg_social_media(df):
+
+    return round(
+        df["daily_social_media_hours"].mean(),
+        2
+    )
+
+
+def avg_sleep(df):
+
+    return round(
+        df["sleep_hours"].mean(),
+        2
+    )
+
+
+def avg_stress(df):
+
+    return round(
+        df["stress_level"].mean(),
+        2
+    )
+
 
 def avg_anxiety(df):
 
     return round(
-        df["Anxiety Score"].mean(),
+        df["anxiety_level"].mean(),
         2
     )
 
-def avg_depression(df):
+
+def avg_addiction(df):
 
     return round(
-        df["Depression Score"].mean(),
+        df["addiction_level"].mean(),
+        2
+    )
+
+
+def depression_rate(df):
+
+    return round(
+        df["depression_label"].mean()*100,
         2
     )
