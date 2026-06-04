71e60ab6d9518066ff6a060e3304795b1a11f2e5
SaiRamC123-com <chutkeychethan@gmail.com>
2026-06-04 13:07:35 +0530

    Update charts.py

--- utils/charts.py
+++ utils/charts.py
@@ -1,1 +1,30 @@
+import plotly.express as px
 
+def age_distribution(df):
+
+    return px.histogram(
+        df,
+        x="Age"
+    )
+
+def anxiety_chart(df):
+
+    return px.bar(
+        df.groupby("Platform")
+        ["Anxiety Score"]
+        .mean()
+        .reset_index(),
+        x="Platform",
+        y="Anxiety Score"
+    )
+
+def depression_chart(df):
+
+    return px.bar(
+        df.groupby("Platform")
+        ["Depression Score"]
+        .mean()
+        .reset_index(),
+        x="Platform",
+        y="Depression Score"
+    )
