060f1205396aa4f8da7e3bbef233d04c31e5e07e
SaiRamC123-com <chutkeychethan@gmail.com>
2026-06-04 13:09:41 +0530

    Update pdf_generator.py

--- reports/pdf_generator.py
+++ reports/pdf_generator.py
@@ -1,1 +1,55 @@
+from fpdf import FPDF
 
+def generate_pdf(summary, insights):
+
+    pdf = FPDF()
+
+    pdf.add_page()
+
+    pdf.set_font(
+        "Arial",
+        "B",
+        18
+    )
+
+    pdf.cell(
+        200,
+        10,
+        "Social Media Impact Report",
+        ln=True,
+        align="C"
+    )
+
+    pdf.ln(10)
+
+    pdf.set_font(
+        "Arial",
+        "",
+        12
+    )
+
+    for key,value in summary.items():
+
+        pdf.cell(
+            200,
+            10,
+            f"{key}: {value}",
+            ln=True
+        )
+
+    pdf.ln(10)
+
+    pdf.multi_cell(
+        0,
+        8,
+        insights
+    )
+
+    path = (
+        "reports/generated_reports/"
+        "mental_health_report.pdf"
+    )
+
+    pdf.output(path)
+
+    return path
