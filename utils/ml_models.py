a8687d22bc76b769ceaad2b608a3e5ea9a677170
SaiRamC123-com <chutkeychethan@gmail.com>
2026-06-04 13:08:00 +0530

    Add model training function using RandomForestClassifier

--- utils/ml_models.py
+++ utils/ml_models.py
@@ -1,1 +1,20 @@
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
 
+def train_model(df):
+
+    X = df[
+        [
+            "Daily Usage Hours",
+            "Sleep Hours",
+            "Age"
+        ]
+    ]
+
+    y = df["Risk"]
+
+    model = RandomForestClassifier()
+
+    model.fit(X,y)
+
+    return model
