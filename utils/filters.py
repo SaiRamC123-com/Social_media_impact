12c9f95773d0bce0f467728b70cab26be8cc70f9
SaiRamC123-com <chutkeychethan@gmail.com>
2026-06-04 13:07:14 +0530

    Update filters.py

--- utils/filters.py
+++ utils/filters.py
@@ -1,1 +1,23 @@
+import streamlit as st
 
+def sidebar_filters(df):
+
+    gender = st.sidebar.multiselect(
+        "Gender",
+        df["Gender"].unique(),
+        default=df["Gender"].unique()
+    )
+
+    platform = st.sidebar.multiselect(
+        "Platform",
+        df["Platform"].unique(),
+        default=df["Platform"].unique()
+    )
+
+    filtered = df[
+        (df["Gender"].isin(gender))
+        &
+        (df["Platform"].isin(platform))
+    ]
+
+    return filtered
