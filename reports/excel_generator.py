c49ece12120e07e13c723026637f9bf38667e439
SaiRamC123-com <chutkeychethan@gmail.com>
2026-06-04 13:09:59 +0530

    Update excel_generator.py

--- reports/excel_generator.py
+++ reports/excel_generator.py
@@ -1,1 +1,42 @@
+import pandas as pd
 
+def generate_excel(df):
+
+    path = (
+        "reports/generated_reports/"
+        "mental_health_report.xlsx"
+    )
+
+    with pd.ExcelWriter(path) as writer:
+
+        df.to_excel(
+            writer,
+            sheet_name="Dataset",
+            index=False
+        )
+
+        summary = pd.DataFrame({
+
+            "Metric":[
+                "Participants",
+                "Avg Usage",
+                "Avg Anxiety",
+                "Avg Depression"
+            ],
+
+            "Value":[
+                len(df),
+                df["Daily Usage Hours"].mean(),
+                df["Anxiety Score"].mean(),
+                df["Depression Score"].mean()
+            ]
+
+        })
+
+        summary.to_excel(
+            writer,
+            sheet_name="Summary",
+            index=False
+        )
+
+    return path
